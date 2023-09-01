0a92558b8e19123dc8a5d4e59b36573bb9ab36e3
halissonmartins <halissonmartins@yahoo.com.br>
2023-08-31 22:19:20 -0300

    new startup messages

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,7 +8,7 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 
 const start = async () => {
 
-  console.log("Starting tickets service...");
+  console.log("Starting Ticket Service...");
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
